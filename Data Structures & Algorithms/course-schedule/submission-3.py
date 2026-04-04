44b6bddd69bfc1cc8e3c59d76c669df7376c1c00
Wiktor Sienkiewicz <88692323+wiksien@users.noreply.github.com>
2026-04-04 22:21:25 +0200

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        def generateIndegreesAndGraph(indegree, prerequisites):
+            for vert, edge in prerequisites:
+                adj[edge].append(vert)
+                indegree[vert] += 1
+        
+        def processZeroIndegree(vert_num):
+            for edge in adj[vert_num]:
+                indegree[edge] -= 1
+                if indegree[edge] == 0:
+                    zero_indegree_queue.append(edge)
+        
+        generateIndegreesAndGraph(indegree, prerequisites)
+        
+        zero_indegree_queue = [i for i in range(numCourses) if indegree[i] == 0]
+        
+        while zero_indegree_queue != []:
+            processZeroIndegree(zero_indegree_queue.pop())
+            numCourses -= 1
+
+        if numCourses == 0:
+            return True
+        else:
+            return False
+        
