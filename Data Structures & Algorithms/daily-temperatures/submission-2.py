8828cc0f868f95509f33602b39e441824469e466
Wiktor Sienkiewicz <88692323+wiksien@users.noreply.github.com>
2026-04-05 23:10:21 +0200

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+
+        for temp_index in range(len(res) - 2, -1, -1):
+            print(res, temp_index)
+            if temperatures[temp_index] < temperatures[temp_index + 1]:
+                res[temp_index] = 1
+            else:
+                hottest_future_temp_index = temp_index + 1
+
+                while temperatures[temp_index] >= temperatures[hottest_future_temp_index]:
+                    if res[hottest_future_temp_index] == 0:
+                        hottest_future_temp_index = temp_index
+                        break
+                    
+                    hottest_future_temp_index += res[hottest_future_temp_index]
+                
+                res[temp_index] = hottest_future_temp_index - temp_index
+            
+        return res
+
